da3adf3d17d99433bcc7cfa9f75be187b24a2013
2dos <2dossrl@gmail.com>
2021-05-20 19:51:47 -0500

    New Randomizer Tab

--- form_data.py
+++ form_data.py
@@ -1,11 +1,14 @@
 """Builds the default page."""
 from browser.template import Template
+from randomizers import Randomizers
 from level_progression import LevelProgression
 from misc import Misc
 
-
+randomizers = Randomizers()
 progression = LevelProgression()
 misc = Misc()
+
+Template("randomizers_tab").render(randomizers=randomizers)
 Template("level_progression_tab").render(progression=progression)
 Template("misc_tab").render(misc=misc)
 Template("spoiler_tab").render()
