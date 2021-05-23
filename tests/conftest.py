d854bf9560439ea957fa3432726071f100817142
Cfox <cfox7.ace@gmail.com>
2021-05-23 02:12:38 -0400

    Implemented quick test case
    Added pytest to the requirements file
    
    Wrote quick test case to make sure flask app opened properly

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from GUI import app as flask_app
+
+@pytest.fixture
+def app():
+	yield flask_app
+
+
+@pytest.fixture
+def client(app):
+	return app.test_client()
---
+++ tests/test_flask.py
@@ -0,0 +1,4 @@
+#Checks that flask opened correctly
+def test_index(app, client):
+	res = client.get('/')
+	assert res.status_code == 200
