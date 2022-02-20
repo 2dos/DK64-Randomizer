8be4493e0cf7bf7b3d7f4a58cbd7e47ba04f5c90
Alex Thieke <athieke9@gmail.com>
2022-02-20 16:03:20 -0500

    Re-organized shuffled_exit_instructions to group loading zones within each container map, to work with ballaam's lz_replacer.py

--- randomizer/Spoiler.py
+++ randomizer/Spoiler.py
@@ -93,21 +93,29 @@ class Spoiler:
     def UpdateExits(self):
         """Update list of shuffled exits."""
         self.shuffled_exit_data = {}
+        containerMaps = {}
         for key, exit in ShufflableExits.items():
             if exit.shuffled:
                 try:
                     vanillaBack = exit.back
                     shuffledBack = ShufflableExits[exit.shuffledId].back
                     self.shuffled_exit_data[key] = shuffledBack
+                    containerMapId = GetMapId(exit.region)
+                    if containerMapId not in containerMaps:
+                        containerMaps[containerMapId] = {
+                            "container_map": containerMapId,  # DK Isles
+                            "zones": [],
+                        }
                     loading_zone_mapping = {}
-                    loading_zone_mapping["container_map"] = GetMapId(exit.region)
-                    loading_zone_mapping["destination_map"] = GetMapId(vanillaBack.regionId)
-                    loading_zone_mapping["destination_exit"] = GetExitId(vanillaBack)
+                    loading_zone_mapping["vanilla_map"] = GetMapId(vanillaBack.regionId)
+                    loading_zone_mapping["vanilla_exit"] = GetExitId(vanillaBack)
                     loading_zone_mapping["new_map"] = GetMapId(shuffledBack.regionId)
                     loading_zone_mapping["new_exit"] = GetExitId(shuffledBack)
-                    self.shuffled_exit_instructions.append(loading_zone_mapping)
+                    containerMaps[containerMapId]["zones"].append(loading_zone_mapping)
                 except Exception as ex:
                     print(ex)
+        for key, containerMap in containerMaps.items():
+            self.shuffled_exit_instructions.append(containerMap)
 
     def UpdateLocations(self, locations):
         """Update location list for what was produced by the fill."""
