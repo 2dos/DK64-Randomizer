430e9b46580dc1cb47b89fee06a834f98fee2e1c
theballaam96 <theballaam96@gmail.com>
2022-02-24 11:51:39 -0600

    Boot Speedup Reversion

--- base-hack/asm/boot.asm
+++ base-hack/asm/boot.asm
@@ -15,11 +15,11 @@ START:
 		LUI v0, 0x8001
 		ADDIU v0, v0, 0xDCC4
 		// Bypass Setup Checks
-		LUI t3, 0x8075
-		ADDIU t4, r0, 1
-		SB t4, 0x00B0 (t3)
-		LUI t3, 0x8074
-		SB t4, 0x7D78 (t3)
+		//LUI t3, 0x8075
+		//ADDIU t4, r0, 1
+		//SB t4, 0x00B0 (t3)
+		//LUI t3, 0x8074
+		//SB t4, 0x7D78 (t3)
 		// Write LZ Update
 		LUI t3, 0x8075
 		SB r0, 0x8E21 (t3)
