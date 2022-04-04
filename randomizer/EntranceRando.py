921ea779ec8696ca13b9c3ba2155136cb44a2026
znernicus <47017338+znernicus@users.noreply.github.com>
2022-04-03 20:19:11 -0400

    Randomize More Loading Zones (#203)
    * Working on additional loading zone rando
    
    * Fixing up how more loading zones are written to ROM (first approach didn't work)
    
    * Fixed mistake on helm exit
    
    * Special case with Factory Crusher due to exit ID not matching as expected
    
    Co-authored-by: Thomas Vatter <tvatter@hotmail.com>

--- randomizer/EntranceRando.py
+++ randomizer/EntranceRando.py
@@ -1,6 +1,8 @@
 """Randomize Entrances passed from Misc options."""
 import js
 
+from randomizer.Enums.Transitions import Transitions
+from randomizer.MapsAndExits import GetExitId, GetMapId, Maps
 from randomizer.Patcher import ROM
 from randomizer.Spoiler import Spoiler
 
@@ -55,10 +57,124 @@ def randomize_entrances(spoiler: Spoiler):
                     lz_exit = int.from_bytes(ROM().readBytes(2), "big")
                     for zone in cont_map["zones"]:
                         if lz_map == zone["vanilla_map"]:
-                            if lz_exit == zone["vanilla_exit"]:
+                            if lz_exit == zone["vanilla_exit"] or (lz_map == Maps.FactoryCrusher):
                                 ROM().seek(cont_map_lzs_address + start + 0x12)
                                 map_bytes = intToArr(zone["new_map"], 2)
                                 ROM().writeBytes(bytearray(map_bytes))
                                 ROM().seek(cont_map_lzs_address + start + 0x14)
                                 exit_bytes = intToArr(zone["new_exit"], 2)
                                 ROM().writeBytes(bytearray(exit_bytes))
+
+        # /* 0x05D */ char randomize_more_loading_zones; // 0 = Not randomizing loading zones inside levels. 1 = On
+        varspaceOffset = 0x1FED020  # TODO: Define this as constant in a more global place
+        moreLoadingZonesOffset = 0x05D
+        ROM().seek(varspaceOffset + moreLoadingZonesOffset)
+        ROM().write(1)
+        # /* 0x05E */ unsigned short aztec_beetle_enter; // Map and exit replacing the loading zone which normally bring you to Aztec Beetle Race from Aztec. First byte is map, second byte is exit value. Same logic applies until (and including) "enter_levels[7]"
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.AztecMainToRace]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x060 */ unsigned short aztec_beetle_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.AztecRaceToMain]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x062 */ unsigned short caves_beetle_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CavesRaceToMain]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x064 */ unsigned short seal_race_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.GalleonSealToShipyard]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x066 */ unsigned short factory_car_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.FactoryRaceToRandD]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x068 */ unsigned short castle_car_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CastleRaceToMuseum]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x06A */ unsigned short seasick_ship_enter; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.GalleonLighthouseAreaToSickBay]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x06C */ unsigned short fungi_minecart_enter; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.ForestMainToCarts]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x06E */ unsigned short fungi_minecart_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.ForestCartsToMain]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x070 */ unsigned short japes_minecart_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.JapesCartsToMain]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x072 */ unsigned short castle_minecart_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CastleCartsToCrypt]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x074 */ unsigned short castle_lobby_enter; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesMainToCastleLobby]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x076 */ unsigned short k_rool_exit; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        # K rool exit is always vanilla
+        ROM().write(Maps.Isles)
+        ROM().write(12)
+        # /* 0x078 */ unsigned short exit_levels[8]; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.JapesToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.AztecToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.FactoryToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.GalleonToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.ForestToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CavesToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CastleToIsles]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # Helm exit is always vanilla
+        ROM().write(Maps.HideoutHelmLobby)
+        ROM().write(1)
+        # /* 0x088 */ unsigned short enter_levels[7]; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToJapes]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToAztec]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToFactory]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToGalleon]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToForest]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToCaves]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.IslesToCastle]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x120 */ unsigned short ballroom_to_museum; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CastleBallroomToMuseum]
+        ROM().seek(varspaceOffset + 0x120)
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
+        # /* 0x122 */ unsigned short museum_to_ballroom; // Same as "aztec_beetle_enter" but for the loading zone dictated by the name
+        shuffledBack = spoiler.shuffled_exit_data[Transitions.CastleMuseumToBallroom]
+        ROM().write(GetMapId(shuffledBack.regionId))
+        ROM().write(GetExitId(shuffledBack))
