f5f62c2afdabf27143bfdfe5d833bb800a7eec0a
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-04-06 01:27:23 -0400

    UI Tweaks

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,10 +1,12 @@
 """Import functions within the UI folder to have them run on load of the UI."""
-from ui.generate_buttons import disable_input, toggle_counts_boxes
+from ui.generate_buttons import disable_input
 from ui.rando_options import (
     update_disabled_progression,
     toggle_loading_zone_coupling,
     set_preset_options,
     preset_select_changed,
+    toggle_b_locker_boxes,
+    toggle_counts_boxes
 )
 
 disable_input(None)
@@ -13,3 +15,4 @@ toggle_loading_zone_coupling(None)
 set_preset_options()
 preset_select_changed(None)
 toggle_counts_boxes(None)
+toggle_b_locker_boxes(None)
