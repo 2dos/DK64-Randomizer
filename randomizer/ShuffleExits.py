a544c8d8ea3236081fe2cb646f50acef9ca607a6
Alex Thieke <athieke9@gmail.com>
2022-04-07 01:30:07 -0400

    Improved logging

--- randomizer/ShuffleExits.py
+++ randomizer/ShuffleExits.py
@@ -246,6 +246,7 @@ def UpdateLevelProgression(settings: Settings):
     for levelIndex in range(len(lobbies)):
         shuffledEntrance = ShufflableExits[LobbyEntrancePool[levelIndex]].shuffledId
         newDestRegion = ShufflableExits[shuffledEntrance].back.regionId
+        print(LobbyEntrancePool[levelIndex].name + " goes to " + newDestRegion.name)
         newIndex = lobbies.index(newDestRegion)
         newEntryGBs[newIndex] = settings.EntryGBs[levelIndex]
         newBossBananas[newIndex] = settings.BossBananas[levelIndex]
