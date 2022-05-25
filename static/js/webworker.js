be3e3e653795c26b5363066c3600495b355b283a
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-05-25 11:41:27 -0400

    deal with locally serving pyodide

--- static/js/webworker.js
+++ static/js/webworker.js
@@ -5,7 +5,7 @@ onmessage = async function (e) {
   // Reload Pyodide
   // TODO: We should probably see about caching this load
   try {
-    importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
+    importScripts("./static/js/pyodide/pyodide.js");
     pyodide = await loadPyodide();
     // Load Micropip so we can load the importers
     await pyodide.loadPackage("micropip");
