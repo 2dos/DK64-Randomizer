a26c4fedbc4d64e1ac1079da5561c08025108c07
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-05-25 21:29:56 -0400

    Update webworker.js

--- static/js/webworker.js
+++ static/js/webworker.js
@@ -5,7 +5,7 @@ onmessage = async function (e) {
   // Reload Pyodide
   // TODO: We should probably see about caching this load
   try {
-    importScripts("./static/js/pyodide/pyodide.js");
+    importScripts("./pyodide/pyodide.js");
     pyodide = await loadPyodide();
     // Load Micropip so we can load the importers
     await pyodide.loadPackage("micropip");
@@ -14,7 +14,7 @@ onmessage = async function (e) {
     await pyodide.runPythonAsync(
       `
       import micropip
-      await micropip.install('pyodide-importer')
+      await micropip.install("` + this.location.origin + `/static/py_libraries/pyodide_importer-0.0.2-py2.py3-none-any.whl")
       from pyodide_importer import register_hook
       register_hook('` +
         this.location.origin +
