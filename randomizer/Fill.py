f4a7ad27ced2c2a521292e1c6535e1c4f642df94
Tom Ballaam <theballaam96@gmail.com>
2022-07-16 15:57:37 -0500

    Bananaport logic

--- randomizer/Fill.py
+++ randomizer/Fill.py
@@ -1413,12 +1413,6 @@ def ShuffleMisc(spoiler):
                     if BananaportVanilla[pair].map_id == map_id and BananaportVanilla[pair].new_warp == warpData.new_warp and BananaportVanilla[pair].name != warpData.name
                 ][0]
                 # Add an exit to each warp's region to the paired warp's region unless it's the same region
-                # or the destination is neither Galleon's Treasure Room nor Aztec's Quicksand Cave, as we do not activate those with this setting
-                if warpData.region_id != pairedWarpData.region_id and pairedWarpData.region_id != Regions.TreasureRoomDiddyGoldTower and pairedWarpData.region_id != Regions.AztecDonkeyQuicksandCave:
-                    warpRegion = Logic.Regions[warpData.region_id]
-                    bananaportExit = TransitionFront(pairedWarpData.region_id, lambda l: True)
-                    # The warp on top of the mountain in Japes is not active until you get the GB
-                    # It does work from the other direction, however, so only this exit is affected
-                    if warpRegion == Regions.JapesTopOfMountain:
-                        bananaportExit.logic = lambda l: Events.JapesDiddySwitch2 in l.Events and l.diddy
-                    warpRegion.exits.append(bananaportExit)
+                warpRegion = Logic.Regions[warpData.region_id]
+                bananaportExit = TransitionFront(pairedWarpData.region_id, lambda l: True)
+                warpRegion.exits.append(bananaportExit)
