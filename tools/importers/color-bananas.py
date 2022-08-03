36144aeb326dbe3b4e85ba15ec489896b34c6f8f
Bismuth <47404842+Bismuth9@users.noreply.github.com>
2022-08-03 17:52:22 -0400

    Add custom Jungle Japes CBs
    Preliminary data for CB rando. Includes custom locations for singles and bunches, but no balloons, no logic and no vanilla bananas.

--- tools/importers/color-bananas.py
+++ tools/importers/color-bananas.py
@@ -4,6 +4,7 @@
 with open("import.csv", newline="") as csvfile:
     dataset = []
     group = None
+    name = None
     map = None
     kongs = {}
     for row in csvfile:
@@ -11,20 +12,24 @@ with open("import.csv", newline="") as csvfile:
         newentry = {}
         if rowdata[0] != "" and rowdata[0] != group:
             group = rowdata[0]
-            map = rowdata[1]
+            name = rowdata[1]
+            map = rowdata[2]
             kongs = {
-                "dk": bool(rowdata[7]),
-                "diddy": bool(rowdata[8]),
-                "lanky": bool(rowdata[9]),
-                "tiny": bool(rowdata[10]),
-                "chunky": bool(rowdata[11]),
+                "dk": bool(int(rowdata[9])),
+                "diddy": bool(int(rowdata[10])),
+                "lanky": bool(int(rowdata[11])),
+                "tiny": bool(int(rowdata[12])),
+                "chunky": bool(int(rowdata[13])),
             }
-            dataset.append({"group": int(group), "map": map, "kongs": kongs, "locations": []})
+            dataset.append({"group": int(group), "map": map, "name": name, "kongs": kongs, "locations": []})
 
-        newentry["amount"] = int(rowdata[2])
-        newentry["x"] = int(float(rowdata[4]))
-        newentry["y"] = int(float(rowdata[5]))
-        newentry["z"] = int(float(rowdata[6]))
+        newentry["amount"] = int(rowdata[3])
+        newentry["scale"] = float(rowdata[5])
+        newentry["x"] = int(float(rowdata[6]))
+        newentry["y"] = int(float(rowdata[7]))
+        newentry["z"] = int(float(rowdata[8]))
         dict_index = next((index for (index, d) in enumerate(dataset) if d["group"] == int(group)), None)
         dataset[dict_index]["locations"].append(newentry)
-    print(dataset)
+
+with open("coloredbananas.txt", "w") as outputfile:
+    outputfile.write(str(dataset))
