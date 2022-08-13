85a7b79e6159a8a8efb8fc0331a4c5a90e6f5e4b
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-08-12 23:46:29 -0400

    Settings Strings

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+"""Wheel File Creation for the website for efficient loading."""
 from setuptools import setup, find_packages
+
 packages = find_packages(exclude=["tests", "base-hack", "build", "dist", "docs", "scripts", "tmp"])
 
 setup(
