979f7b0499acde82bcfc44fe5ddd8257d8e474a1
Bismuth <47404842+Bismuth9@users.noreply.github.com>
2022-08-14 22:29:57 -0400

    Add vanilla bananas for Japes/Aztec

--- randomizer/Lists/CBLocations/GloomyGalleonCBLocations.py
+++ randomizer/Lists/CBLocations/GloomyGalleonCBLocations.py
@@ -43,7 +43,7 @@ class Balloon:
         self.spawnPoint = self.setSpawnPoint(points)
 
     def setSpawnPoint(self, points=[]):
-        """Set the spawn point of a balloon based on its path"""
+        """Set the spawn point of a balloon based on its path."""
         spawnX = 0
         spawnY = 0
         spawnZ = 0
