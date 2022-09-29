e7f3bb7a1ecec21e1dda75060d75d97daf39257d
Tom Ballaam <theballaam96@gmail.com>
2022-09-29 15:12:23 -0500

    Initial dump

---
+++ randomizer/Patching/CrownPlacer.py
@@ -0,0 +1,10 @@
+import js
+from randomizer.Patching.Patcher import ROM
+from randomizer.Enums.Levels import Levels
+from randomizer.Spoiler import Spoiler
+from randomizer.Patching.Lib import float_to_hex, short_to_ushort
+
+from randomizer.Lists.CrownLocations import CrownLocations
+
+def randomize_crown_pads(spoiler: Spoiler):
+    # Do memes
