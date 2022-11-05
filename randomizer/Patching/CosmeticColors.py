b5917387b9310ba2b2d2026ec6667310a299eb96
AlmostSeagull <107404587+AlmostSeagull@users.noreply.github.com>
2022-11-05 20:35:51 +0100

    3 colors means no need for 2-color catch

--- randomizer/Patching/CosmeticColors.py
+++ randomizer/Patching/CosmeticColors.py
@@ -429,38 +429,6 @@ def overwrite_object_colors(spoiler: Spoiler):
                     balloon_im.paste(dk_single, balloon_single_frames[file - 5819], dk_single)
                     balloon_start = [5835, 5827, 5843, 5851, 5819]
                     writeColorImageToROM(balloon_im, 25, balloon_start[kong_index] + (file - 5819), 32, 64)
-            elif mode == "prot-deut" and kong_index < (colors_amount + 1):
-                # file = 4120
-                # # Kasplat Hair
-                # hair_im = getFile(25, file, True, 32, 44)
-                # hair_im = maskImage(hair_im, kong_index, 0)
-                writeColorToROM(color_bases[kong_index], 25, [4124, 4122, 4123, 4120, 4121][kong_index])
-                # writeColorImageToROM(hair_im, 25, [4124, 4122, 4123, 4120, 4121][kong_index], 32, 44)
-                for file in range(152, 160):
-                    # Single
-                    single_im = getFile(7, file, False, 44, 44)
-                    single_im = maskImage(single_im, kong_index, 0)
-                    single_start = [168, 152, 232, 208, 240]
-                    writeColorImageToROM(single_im, 7, single_start[kong_index] + (file - 152), 44, 44)
-                for file in range(216, 224):
-                    # Coin
-                    coin_im = getFile(7, file, False, 48, 42)
-                    coin_im = maskImage(coin_im, kong_index, 0)
-                    coin_start = [224, 256, 248, 216, 264]
-                    writeColorImageToROM(coin_im, 7, coin_start[kong_index] + (file - 216), 48, 42)
-                for file in range(274, 286):
-                    # Bunch
-                    bunch_im = getFile(7, file, False, 44, 44)
-                    bunch_im = maskImage(bunch_im, kong_index, 0)
-                    bunch_start = [274, 854, 818, 842, 830]
-                    writeColorImageToROM(bunch_im, 7, bunch_start[kong_index] + (file - 274), 44, 44)
-                for file in range(5819, 5827):
-                    # Balloon
-                    balloon_im = getFile(25, file, True, 32, 64)
-                    balloon_im = maskImage(balloon_im, kong_index, 33)
-                    balloon_im.paste(dk_single, balloon_single_frames[file - 5819], dk_single)
-                    balloon_start = [5835, 5827, 5843, 5851, 5819]
-                    writeColorImageToROM(balloon_im, 25, balloon_start[kong_index] + (file - 5819), 32, 64)
             else: 
                 # file = 4120
                 # # Kasplat Hair
