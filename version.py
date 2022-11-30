68f2f947ba31a48c26ec84a26bc5a2d020b1d2b6
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2022-11-30 12:38:08 -0500

    migrate to external process

--- version.py
+++ version.py
@@ -1,6 +1,5 @@
 """Holds the version for DK64 Rando."""
 import js
-from hashlib import md5
 
 stable_version = "1.5"
 dev_version = "2.0"
@@ -27,8 +26,4 @@ try:
     js.document.getElementById("live-version").text = current_version + " | "
 except Exception:
     pass
-try:
-    resp = js.getFile("./static/py_libraries/dk64rando-1.0.0-py3-none-any.whl")
-    whl_hash = md5(resp).hexdigest()
-except Exception as e:
-    whl_hash = "no_file_using_filler_hash"
+whl_hash = "no_file_using_filler_hash"
