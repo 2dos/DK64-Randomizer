b16ad121933099b3a54e2516d995cb778202f370
Tom Ballaam <theballaam96@gmail.com>
2022-12-15 15:17:08 -0600

    Lint overlords

--- randomizer/LogicFiles/HideoutHelm.py
+++ randomizer/LogicFiles/HideoutHelm.py
@@ -15,12 +15,8 @@ LogicRegions = {
     Regions.HideoutHelmStart: Region("Hideout Helm Start", "Hideout Helm", Levels.HideoutHelm, True, None, [], [], [
         TransitionFront(Regions.HideoutHelmLobby, lambda l: True),
         TransitionFront(Regions.HideoutHelmMain, lambda l: l.settings.helm_setting == "skip_start" or (l.handstand and l.lanky and ((l.pineapple and l.chunky and l.vines and ((l.mini and l.tiny) or l.generalclips)) or (l.CanMoonkick() or l.phasewalk or l.CanOStandTBSNoclip())))),
-        TransitionFront(Regions.HideoutHelmDonkeyRoom, lambda l: 
-            l.settings.helm_setting == "default" and (l.handstand and l.lanky and (l.CanMoonkick() or l.phasewalk or l.CanOStandTBSNoclip() or (l.pineapple and l.chunky and l.vines and l.generalclips))) # Switch Kick/T1 Clip
-        ),
-        TransitionFront(Regions.HideoutHelmChunkyRoom, lambda l: 
-            l.settings.helm_setting == "default" and (l.handstand and l.lanky and l.CanMoonkick()) # Switch Kick/T1 Clip
-        ),
+        TransitionFront(Regions.HideoutHelmDonkeyRoom, lambda l: l.settings.helm_setting == "default" and (l.handstand and l.lanky and (l.CanMoonkick() or l.phasewalk or l.CanOStandTBSNoclip() or (l.pineapple and l.chunky and l.vines and l.generalclips)))),
+        TransitionFront(Regions.HideoutHelmChunkyRoom, lambda l: l.settings.helm_setting == "default" and (l.handstand and l.lanky and l.CanMoonkick())),
         TransitionFront(Regions.HideoutHelmAfterBoM, lambda l: l.settings.helm_setting == "skip_all"),
     ]),
 
@@ -35,7 +31,7 @@ LogicRegions = {
         LocationLogic(Locations.HelmLanky1, lambda l: l.settings.helm_setting == "skip_all", MinigameType.HelmBarrel),
         LocationLogic(Locations.HelmLanky2, lambda l: l.settings.helm_setting == "skip_all", MinigameType.HelmBarrel),
         LocationLogic(Locations.HelmDiddy1, lambda l: l.settings.helm_setting == "skip_all", MinigameType.HelmBarrel),
-        LocationLogic(Locations.HelmDiddy2, lambda l: l.settings.helm_setting == "skip_all", MinigameType.HelmBarrel),        
+        LocationLogic(Locations.HelmDiddy2, lambda l: l.settings.helm_setting == "skip_all", MinigameType.HelmBarrel),
     ], [
         Event(Events.HelmDoorsOpened, lambda l: (l.grab and l.donkey and l.jetpack and l.diddy and l.punch and l.chunky) or l.settings.helm_setting != "default"),
         Event(Events.HelmDonkeyDone, lambda l: ((l.isPriorHelmComplete(Kongs.donkey) or l.settings.helm_setting == "skip_all") and l.HelmDonkey1 and l.HelmDonkey2) or not l.settings.helm_donkey),
@@ -43,15 +39,13 @@ LogicRegions = {
         Event(Events.HelmTinyDone, lambda l: ((l.isPriorHelmComplete(Kongs.tiny) or l.settings.helm_setting == "skip_all") and l.HelmTiny1 and l.HelmTiny2) or not l.settings.helm_tiny),
         Event(Events.HelmLankyDone, lambda l: ((l.isPriorHelmComplete(Kongs.lanky) or l.settings.helm_setting == "skip_all") and l.HelmLanky1 and l.HelmLanky2) or not l.settings.helm_lanky),
         Event(Events.HelmDiddyDone, lambda l: ((l.isPriorHelmComplete(Kongs.diddy) or l.settings.helm_setting == "skip_all") and l.HelmDiddy1 and l.HelmDiddy2) or not l.settings.helm_diddy),
-        
     ], [
         TransitionFront(Regions.HideoutHelmDonkeyRoom, lambda l: (l.bongos and l.isdonkey and Events.HelmDoorsOpened in l.Events) or l.phasewalk),
         TransitionFront(Regions.HideoutHelmChunkyRoom, lambda l: (l.triangle and l.ischunky and Events.HelmDoorsOpened in l.Events) or l.phasewalk),
         TransitionFront(Regions.HideoutHelmTinyRoom, lambda l: (l.saxophone and l.istiny and Events.HelmDoorsOpened in l.Events) or l.phasewalk),
         TransitionFront(Regions.HideoutHelmLankyRoom, lambda l: (l.trombone and l.islanky and Events.HelmDoorsOpened in l.Events) or l.phasewalk),
         TransitionFront(Regions.HideoutHelmDiddyRoom, lambda l: l.isdiddy and l.jetpack and ((Events.HelmDoorsOpened in l.Events and l.guitar) or l.phasewalk)),
-        TransitionFront(Regions.HideoutHelmAfterBoM, lambda l: l.settings.helm_setting == "skip_all" or (Events.HelmDonkeyDone in l.Events and Events.HelmChunkyDone in l.Events
-              and Events.HelmTinyDone in l.Events and Events.HelmLankyDone in l.Events and Events.HelmDiddyDone in l.Events)),
+        TransitionFront(Regions.HideoutHelmAfterBoM, lambda l: l.settings.helm_setting == "skip_all" or (Events.HelmDonkeyDone in l.Events and Events.HelmChunkyDone in l.Events and Events.HelmTinyDone in l.Events and Events.HelmLankyDone in l.Events and Events.HelmDiddyDone in l.Events)),
     ]),
 
     Regions.HideoutHelmDonkeyRoom: Region("Hideout Helm Main", "Hideout Helm", Levels.HideoutHelm, False, -1, [
@@ -61,7 +55,7 @@ LogicRegions = {
     ], [], [
         TransitionFront(Regions.HideoutHelmMain, lambda l: True),
     ]),
-    
+
     Regions.HideoutHelmChunkyRoom: Region("Hideout Helm Main", "Hideout Helm", Levels.HideoutHelm, False, -1, [
         LocationLogic(Locations.HelmChunky1, lambda l: True, MinigameType.HelmBarrel),
         LocationLogic(Locations.HelmChunky2, lambda l: True, MinigameType.HelmBarrel),
@@ -94,13 +88,10 @@ LogicRegions = {
         LocationLogic(Locations.HelmKey, lambda l: Events.HelmKeyAccess in l.Events),
         LocationLogic(Locations.HelmBananaFairy1, lambda l: l.camera and Events.HelmKeyAccess in l.Events),
         LocationLogic(Locations.HelmBananaFairy2, lambda l: l.camera and Events.HelmKeyAccess in l.Events),
-    ],
-    [
+    ], [
         Event(Events.HelmKeyAccess, lambda l: ((l.settings.crown_door_open or l.BattleCrowns >= 4) and (l.settings.coin_door_open or l.nintendoCoin and l.rarewareCoin)) or l.generalclips),
-    ],
-    [
-        TransitionFront(Regions.HideoutHelmMain, lambda l: l.settings.helm_setting == "skip_all" or (Events.HelmDonkeyDone in l.Events and Events.HelmChunkyDone in l.Events
-              and Events.HelmTinyDone in l.Events and Events.HelmLankyDone in l.Events and Events.HelmDiddyDone in l.Events) or (l.generalclips or l.phasewalk)),
+    ], [
+        TransitionFront(Regions.HideoutHelmMain, lambda l: l.settings.helm_setting == "skip_all" or (Events.HelmDonkeyDone in l.Events and Events.HelmChunkyDone in l.Events and Events.HelmTinyDone in l.Events and Events.HelmLankyDone in l.Events and Events.HelmDiddyDone in l.Events) or (l.generalclips or l.phasewalk)),
         TransitionFront(Regions.HideoutHelmDonkeyRoom, lambda l: l.generalclips or l.phasewalk),
         TransitionFront(Regions.HideoutHelmChunkyRoom, lambda l: l.generalclips or l.phasewalk),
         TransitionFront(Regions.HideoutHelmLankyRoom, lambda l: l.generalclips or l.phasewalk),
