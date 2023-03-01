4a037d6f063a3aee606680acfe1bfff9f642d865
Tom Ballaam <theballaam96@gmail.com>
2023-03-01 07:53:10 -0600

    Update colors

--- base-hack/src/initialization/text.c
+++ base-hack/src/initialization/text.c
@@ -11,12 +11,13 @@
 
 #include "../../include/common.h"
 
-static unsigned int base_text_color = 0x2828FF00;
+static unsigned int base_text_color = 0x00000000;
 static unsigned int emph_text_colors[] = {
+    0xA3620000,
+    0xB0000000,
+    0x2828FF00,
     0x8000FF00,
-    0x8000FF00,
-    0x8000FF00,
-    0x8000FF00,
+    0x00800000,
 };
 
 typedef struct text_params {
@@ -38,7 +39,7 @@ int* displayModifiedText(int* dl, int style, int x, int y, char* text, text_para
     int dl_old = (int)dl;
     dl_old -= 4;
     *(unsigned int*)(dl_old) = base_text_color | (*(int*)(dl_old) & 0xFF);
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         if (params->effect_bitfield & (0x10 << i)) {
             *(unsigned int*)(dl_old) = emph_text_colors[i] | (*(int*)(dl_old) & 0xFF);
         }
@@ -56,6 +57,12 @@ void initTextChanges(void) {
         base_text_color = 0xFFFFFF00;
         emph_text_colors[0] = 0xFFA01000;
         emph_text_colors[1] = 0xFF000000;
+        emph_text_colors[2] = 0x0C7DED00;
+        emph_text_colors[3] = 0xBB1CFF00;
+        emph_text_colors[4] = 0x59FF6400;
+    } else {
+        float opacity = 200.0f;
+        *(short*)(0x806A45C6) = *(short*)(&opacity);
     }
     // Text
     *(int*)(0x806A3B38) = 0xAFB10014; // Pass in effect bitfield as the last arg
