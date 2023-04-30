20a257d9c6bd2c44e05ec4481dca41c5bf7a3b22
Tom Ballaam <theballaam96@gmail.com>
2023-04-30 15:04:06 -0500

    Song recategorization

--- ui/rando_options.py
+++ ui/rando_options.py
@@ -507,7 +507,7 @@ def disable_music(evt):
     disabled = False
     if js.document.getElementById("random_music").checked:
         disabled = True
-    for i in ["bgm", "fanfares", "events"]:
+    for i in ["bgm", "pickups", "events"]:
         music = js.document.getElementById(f"music_{i}_randomized")
         try:
             if disabled:
