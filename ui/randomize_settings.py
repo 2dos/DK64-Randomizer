c91037071821fcca397a41ea2a1dff401ec87229
Jim Avery <thesounddefense@gmail.com>
2023-11-04 20:00:12 -0500

    Initial commit

---
+++ ui/randomize_settings.py
@@ -0,0 +1,9 @@
+"""Function to randomize settings and store them in a dictionary."""
+
+
+def randomize_settings():
+    """Generate random settings based on provided weight.
+    
+    Returns:
+        dict: Dictionary of form settings.
+    """
