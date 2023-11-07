e0c323536f7624ecd9c3dad422106f6906f7be7a
AlmostSeagull <107404587+AlmostSeagull@users.noreply.github.com>
2023-11-07 18:17:05 +0100

    krusha cant jump

--- randomizer/Lists/BananaCoinLocations.py
+++ randomizer/Lists/BananaCoinLocations.py
@@ -231,7 +231,7 @@ BananaCoinGroupList = {
             region=Regions.KremIsle,
             logic=lambda l: (l.hasMoveSwitchsanity(Switches.IslesMonkeyport) and ((l.istiny and l.twirl) or (l.settings.damage_amount != DamageAmount.ohko)))
             or (l.GalleonKey and Events.IslesDiddyBarrelSpawn in l.Events and l.isdiddy and l.jetpack)
-            or (l.AztecKey and (l.isdonkey and (l.settings.damage_amount != DamageAmount.ohko)) or (l.istiny and l.twirl)),
+            or (l.AztecKey and (l.isdonkey and l.settings.krusha_kong != Kongs.donkey and (l.settings.damage_amount != DamageAmount.ohko)) or (l.istiny and l.twirl)),
             locations=[
                 [1.0, 3343, 712, 3376],
                 [1.0, 3318, 710, 3427],
