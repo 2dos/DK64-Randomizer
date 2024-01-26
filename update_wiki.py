8d7fb5334319a52ecd34ace18757ec6af60db279
Tom Ballaam <theballaam96@gmail.com>
2024-01-25 20:42:31 -0600

    VSCode Task

--- update_wiki.py
+++ update_wiki.py
@@ -3,13 +3,16 @@ import os
 import json
 from enum import IntEnum, auto
 
+
 class CamelCaseState(IntEnum):
     """Camel Case State enum."""
+
     null = auto()
     lower = auto()
     upper = auto()
     space = auto()
     number = auto()
+
 
 def getNameFromCamelSnakeCase(text: str):
     """Convert camel or snake case text into regular text."""
@@ -43,13 +46,12 @@ def getNameFromCamelSnakeCase(text: str):
             last_state = CamelCaseState.null
     return output_string
 
+
 def createArticleJSON(file_heads: list):
     """Create JSON file containing all articles."""
     with open("./wiki/articles.json", "w") as fh:
-        fh.write(json.dumps([{
-            "name": getNameFromCamelSnakeCase(x),
-            "link": x
-        } for x in file_heads], indent=4))
+        fh.write(json.dumps([{"name": getNameFromCamelSnakeCase(x), "link": x} for x in file_heads], indent=4))
+
 
 def createHTML(markdown_file_name: str, markdown_path: str, template_text: str):
     """Create the relevant HTML file for a markdown entry."""
@@ -58,18 +60,19 @@ def createHTML(markdown_file_name: str, markdown_path: str, template_text: str):
     html_file_name = f"{file_head}.html"
     html_text = template_text.replace("<title></title>", f"<title>{getNameFromCamelSnakeCase(entry_name)}</title>")
     html_text = template_text.replace("<title></title>", f"<title>{getNameFromCamelSnakeCase(entry_name)}</title>")
-    html_text = template_text.replace("<h1 id=\"page-title\"></h1>", f"<h1 id=\"page-title\">{getNameFromCamelSnakeCase(entry_name)}</h1>")
-    html_text = html_text.replace("<div id=\"markdown_content\"></div>", f"<div id=\"markdown_content\" ref=\"{markdown_path.replace('./','./article_markdown/')}\"></div>")
+    html_text = template_text.replace('<h1 id="page-title"></h1>', f'<h1 id="page-title">{getNameFromCamelSnakeCase(entry_name)}</h1>')
+    html_text = html_text.replace('<div id="markdown_content"></div>', f"<div id=\"markdown_content\" ref=\"{markdown_path.replace('./','./article_markdown/')}\"></div>")
     with open(f"./wiki/{html_file_name}", "w") as fh:
         fh.write("<!-- DON'T EDIT THIS FILE. EDIT template.html INSTEAD -->\n")
         fh.write(html_text)
+
 
 items = []
 ARTICLE_DIRECTORY = "./wiki/article_markdown"
 for path, subdirs, files in os.walk(ARTICLE_DIRECTORY):
     for name in files:
         if ".md" in name:
-            items.append(os.path.join(path, name).replace(ARTICLE_DIRECTORY, ".").replace("\\","/"))
+            items.append(os.path.join(path, name).replace(ARTICLE_DIRECTORY, ".").replace("\\", "/"))
 template_text = None
 with open("./wiki/template.html", "r") as fh:
     template_text = fh.read()
