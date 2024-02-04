8f8b85758303caaf8d8b1b298d325261c077b99d
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2024-02-03 20:12:53 -0500

    Update ApplyRandomizer.py

--- randomizer/Patching/ApplyRandomizer.py
+++ randomizer/Patching/ApplyRandomizer.py
@@ -589,7 +589,7 @@ def patching_response(spoiler):
     writeBootMessages()
     enableTriggerText(spoiler)
     shortenCastleMinecart(spoiler)
-    
+
     if "PYTEST_CURRENT_TEST" not in os.environ:
         replaceIngameText(spoiler)
         updateMillLeverTexture(spoiler.settings)
@@ -609,22 +609,24 @@ def patching_response(spoiler):
 
     # Create a dummy time to attach to the end of the file name non decimal
     str(time.time()).replace(".", "")
+    if "PYTEST_CURRENT_TEST" not in os.environ:
+        created_tempfile = mktemp()
+        delta_tempfile = mktemp()
+        # Write the LocalROM.rom bytesIo to a file
+        with open(created_tempfile, "wb") as f:
+            f.write(ROM_COPY.rom.getvalue())
 
-    created_tempfile = mktemp()
-    delta_tempfile = mktemp()
-    # Write the LocalROM.rom bytesIo to a file
-    with open(created_tempfile, "wb") as f:
-        f.write(ROM_COPY.rom.getvalue())
+        import pyxdelta
 
-    import pyxdelta
-
-    pyxdelta.run("dk64.z64", created_tempfile, delta_tempfile)
-    # Read the patch file
-    with open(delta_tempfile, "rb") as f:
-        patch = f.read()
-    # Delete the patch.z64 file
-    os.remove(created_tempfile)
-    os.remove(delta_tempfile)
+        pyxdelta.run("dk64.z64", created_tempfile, delta_tempfile)
+        # Read the patch file
+        with open(delta_tempfile, "rb") as f:
+            patch = f.read()
+        # Delete the patch.z64 file
+        os.remove(created_tempfile)
+        os.remove(delta_tempfile)
+    else:
+        patch = None
     return patch
 
 
