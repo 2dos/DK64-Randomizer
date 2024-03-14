7818d7af5f87fc73e00d2ee439eefb5072c44653
AlmostSeagull <107404587+AlmostSeagull@users.noreply.github.com>
2024-03-14 22:18:33 +0100

    Shed some light on this
    - The background music formerly indicated as "Fungi Forest (Winch)" has been renamed to "Creepy Castle (Shed)" because that's where it plays.

--- randomizer/Lists/Songs.py
+++ randomizer/Lists/Songs.py
@@ -113,7 +113,6 @@ song_data = {
     Songs.ForestMushroomRooms: Song(
         "Fungi Forest (Mushroom Top Rooms)", mem_idx=98, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy, SongGroup.Calm]
     ),
-    Songs.ForestWinch: Song("Fungi Forest (Winch)", mem_idx=139, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy]),
     Songs.ForestSpider: Song("Fungi Forest (Spider)", mem_idx=97, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Fight], mood_tags=[SongGroup.Happy, SongGroup.Gloomy]),
     Songs.ForestBlast: Song("Fungi Forest (Baboon Blast)", mem_idx=164, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Exteriors], mood_tags=[SongGroup.Gloomy, SongGroup.Calm]),
     Songs.ForestRabbitRace: Song("Fungi Forest (Rabbit Race)", mem_idx=169, type=SongType.BGM, memory=0x188, location_tags=[SongGroup.Minigames], mood_tags=[SongGroup.Happy]),
@@ -131,6 +130,7 @@ song_data = {
     Songs.CavesDillo: Song("Crystal Caves (Army Dillo)", mem_idx=148, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Fight], mood_tags=[SongGroup.Happy, SongGroup.Gloomy]),
     # Creepy Castle BGM
     Songs.Castle: Song("Creepy Castle", mem_idx=105, type=SongType.BGM, memory=0x101, location_tags=[SongGroup.Exteriors], mood_tags=[SongGroup.Gloomy, SongGroup.Calm]),
+    Songs.CastleShed: Song("Creepy Castle (Shed)", mem_idx=139, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy]),
     Songs.CastleTree: Song("Creepy Castle (Tree)", mem_idx=141, type=SongType.BGM, memory=0x101, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy]),
     Songs.CastleTunnels: Song("Creepy Castle (Tunnels)", mem_idx=121, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy, SongGroup.Calm]),
     Songs.CastleCrypt: Song("Creepy Castle (Crypt)", mem_idx=126, type=SongType.BGM, memory=0x100, location_tags=[SongGroup.Interiors], mood_tags=[SongGroup.Gloomy, SongGroup.Calm]),
@@ -404,7 +404,7 @@ song_idx_list = [
     song_data[Songs.CastleBallroom],
     song_data[Songs.CastleGreenhouse],
     song_data[Songs.KRoolTheme],
-    song_data[Songs.ForestWinch],
+    song_data[Songs.CastleShed],
     song_data[Songs.CastleTower],
     song_data[Songs.CastleTree],
     song_data[Songs.CastleMuseum],
@@ -507,7 +507,6 @@ FungiForestSongs = {
     Songs.ForestAnthill,
     Songs.ForestMushroom,
     Songs.ForestMushroomRooms,
-    Songs.ForestWinch,
     Songs.ForestSpider,
     Songs.ForestBlast,
     Songs.ForestRabbitRace,
@@ -527,6 +526,7 @@ CrystalCavesSongs = {
 }
 CreepyCastleSongs = {
     Songs.Castle,
+    Songs.CastleShed,
     Songs.CastleTree,
     Songs.CastleTunnels,
     Songs.CastleCrypt,
