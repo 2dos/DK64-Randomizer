1e3f9dcdf5e3a555c924eb372196a83dae0b8dbd
Tom Ballaam <theballaam96@gmail.com>
2024-06-30 12:45:00 -0500

    Item Pool

--- cli.py
+++ cli.py
@@ -41,7 +41,7 @@ def generate(generate_settings, file_name):
                 "spoiler_log": str(json.dumps(spoiler_log)),
             }
         )
-    sections_to_retain = ["Settings", "Cosmetics", "Spoiler Hints", "Spoiler Hints Human Readable"]
+    sections_to_retain = ["Settings", "Cosmetics", "Spoiler Hints", "Spoiler Hints Human Readable", "Item Pool"]
     if spoiler.settings.generate_spoilerlog is False:
         spoiler_log = {k: v for k, v in spoiler_log.items() if k in sections_to_retain}
 
