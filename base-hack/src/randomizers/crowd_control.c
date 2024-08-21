52d9750cafcce90d547d4d64b5ea79764de05dcb
Tom Ballaam <theballaam96@gmail.com>
2024-08-21 09:34:20 -0500

    Coin/GB effects, get SDK set up

--- base-hack/src/randomizers/crowd_control.c
+++ base-hack/src/randomizers/crowd_control.c
@@ -9,9 +9,7 @@ static cc_effects effect_data;
         - Also makes the camera wobble. This can be disabled with "Remove Water Oscillation" on the website
     
     Not Yet implemented effects:
-    - Disable TA
     - Warp to the DK Rap
-    - Caves Stalactite Fall (anywhere)
 */
 
 typedef struct cc_effect_data {
@@ -57,9 +55,6 @@ int cc_enabler_icetrap(void) {
 }
 
 int cc_allower_icetrap(void) {
-    if (!cc_allower_generic()) {
-        return 0;
-    }
     return ice_trap_queued == ICETRAP_OFF;
 }
 
@@ -75,9 +70,6 @@ typedef struct actor_init_data {
 } actor_init_data;
 
 int cc_allower_rockfall(void) {
-    if (!cc_allower_generic()) {
-        return 0;
-    }
     return LoadedActorCount < 50; // Not safe to add it
 }
 
@@ -94,11 +86,102 @@ int cc_enabler_rockfall(void) {
     return spawnActorSpawnerContainer(0x5C, *(int*)(&x), *(int*)(&y), *(int*)(&z), 0, 0x3F000000, 0, &unk);
 }
 
+#define COIN_DELTA 2
+
+int cc_allower_givecoins(void) {
+    for (int i = 0; i < 5; i++) {
+        if (MovesBase[i].coins < 255) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int cc_enabler_givecoins(void) {
+    for (int i = 0; i < 5; i++) {
+        if (MovesBase[i].coins < (255 - COIN_DELTA)) {
+            MovesBase[i].coins += COIN_DELTA;
+        } else {
+            MovesBase[i].coins = 255;
+        }
+    }
+    setPermFlag(FLAG_FIRST_COIN_COLLECTION);
+    return 1;
+}
+
+int cc_allower_removecoins(void) {
+    for (int i = 0; i < 5; i++) {
+        if (MovesBase[i].coins > 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int cc_enabler_removecoins(void) {
+    for (int i = 0; i < 5; i++) {
+        if (MovesBase[i].coins >= COIN_DELTA) {
+            MovesBase[i].coins -= COIN_DELTA;
+        } else {
+            MovesBase[i].coins = 0;
+        }
+    }
+    return 1;
+}
+
+#define GB_CAP_COUNT
+
+int cc_enabler_givegb(void) {
+    int min_kong = -1;
+    int min_level = -1;
+    int min_gb = 999999;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (MovesBase[i].gb_count[j] < min_gb) {
+                min_gb = MovesBase[i].gb_count[j];
+                min_kong = i;
+                min_level = j;
+            }
+        }
+    }
+    MovesBase[min_kong].gb_count[min_level] += 1;
+    return 1;
+}
+
+int cc_allower_removegb(void) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (MovesBase[i].gb_count[j] > 0) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int cc_enabler_removegb(void) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (MovesBase[i].gb_count[j] > 0) {
+                MovesBase[i].gb_count[j] -= 1;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
 static const cc_effect_data cc_funcs[] = {
-    {.enabler = &cc_enable_drunky, .disabler = &cc_disable_drunky, .allower = &cc_allower_generic, .restart_upon_map_entry = 1}, // Drunky Kong
+    {.enabler = &cc_enable_drunky, .disabler = &cc_disable_drunky, .restart_upon_map_entry = 1}, // Drunky Kong
     {.restart_upon_map_entry = 0}, // Disable Tag Anywhere
     {.enabler = &cc_enabler_icetrap, .allower=&cc_allower_icetrap, .auto_disable = 1}, // Ice Trap
     {.enabler = &cc_enabler_rockfall, .allower=&cc_allower_rockfall, .active = 1}, // Rockfall
+    {.enabler = &cc_enabler_givecoins, .allower=&cc_allower_givecoins, .auto_disable = 1}, // Give Coins
+    {.enabler = &cc_enabler_removecoins, .allower=&cc_allower_removecoins, .auto_disable = 1}, // Remove Coins
+    {.enabler = &cc_enabler_givegb, .auto_disable = 1}, // Give GB
+    {.enabler = &cc_enabler_removegb, .allower=&cc_allower_removegb, .auto_disable = 1}, // Remove GB
 };
 
 void cc_effect_handler(void) {
@@ -110,10 +193,12 @@ void cc_effect_handler(void) {
         switch (state) {
             case CC_READY:
             case CC_LOCKED:
-                if (cc_funcs[i].allower) {
-                    if (!callFunc(cc_funcs[i].allower)) {
-                        *eff_data = CC_LOCKED;
-                        break;
+                if (cc_allower_generic()) {
+                    if (cc_funcs[i].allower) {
+                        if (!callFunc(cc_funcs[i].allower)) {
+                            *eff_data = CC_LOCKED;
+                            break;
+                        }
                     }
                 }
                 if (state == CC_LOCKED) {
@@ -126,10 +211,12 @@ void cc_effect_handler(void) {
                     break;
                 }
                 if (cc_funcs[i].active) {
-                    if (cc_funcs[i].allower) {
-                        if (callFunc(cc_funcs[i].allower)) {
-                            if (cc_funcs[i].enabler) {
-                                callFunc(cc_funcs[i].enabler);
+                    if (cc_allower_generic()) {
+                        if (cc_funcs[i].allower) {
+                            if (callFunc(cc_funcs[i].allower)) {
+                                if (cc_funcs[i].enabler) {
+                                    callFunc(cc_funcs[i].enabler);
+                                }
                             }
                         }
                     }
@@ -155,10 +242,12 @@ void cc_effect_handler(void) {
                         return;
                     }
                 }
-                if (cc_funcs[i].allower) {
-                    if (!callFunc(cc_funcs[i].allower)) {
-                        *eff_data = CC_LOCKED;
-                        break;
+                if (cc_allower_generic()) {
+                    if (cc_funcs[i].allower) {
+                        if (!callFunc(cc_funcs[i].allower)) {
+                            *eff_data = CC_LOCKED;
+                            break;
+                        }
                     }
                 }
                 *eff_data = CC_READY;
---
+++ crowd_control/SDK/DonkeyKong64Randomizer.cs
@@ -0,0 +1,122 @@
+﻿using ConnectorLib;
+using ConnectorLib.Memory;
+using CrowdControl.Common;
+using JetBrains.Annotations;
+using ConnectorType = CrowdControl.Common.ConnectorType;
+
+namespace CrowdControl.Games.Packs.DonkeyKong64Randomizer;
+
+[UsedImplicitly]
+public class DonkeyKong64Randomizer : N64EffectPack
+{
+    //private DomainMappedConnector RDRAM;
+
+    public DonkeyKong64Randomizer(UserRecord player, Func<CrowdControlBlock, bool> responseHandler,
+        Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler)
+    {
+        ConnectorAttached += (_, _) => Init();
+    }
+
+    private void Init()
+    {
+        //RDRAM = new(Connector, "RDRAM");
+        DRUNKY_STATE = AddressChain.Begin(Connector).Move(0x807FFFB4).Follow(4, Endianness.BigEndian, PointerType.Absolute);
+        MAP_TIMER = AddressChain.Begin(Connector).Move(0x8076A064); // u32
+        PLAYER_POINTER = AddressChain.Begin(Connector).Move(0x807FBB4C); // u32
+        AUTOWALK_STATE = AddressChain.Begin(Connector).Move(0x807463B8); // u8
+        CURRENT_GAMEMODE = AddressChain.Begin(Connector).Move(0x80755314); // u8
+        NEXT_GAMEMODE = AddressChain.Begin(Connector).Move(0x80755318); // u8
+    }
+
+    private AddressChain DRUNKY_STATE;
+    private AddressChain MAP_TIMER;
+    private AddressChain PLAYER_POINTER;
+    private AddressChain AUTOWALK_STATE;
+    private AddressChain CURRENT_GAMEMODE;
+    private AddressChain NEXT_GAMEMODE;
+    private enum CC_STATE
+    {
+        CC_READY, // 0
+        CC_ENABLING, // 1
+        CC_ENABLED, // 2
+        CC_DISABLING, // 3
+        CC_LOCKED, // 4
+    }
+
+    public override Game Game { get; } = new("Donkey Kong 64 Randomizer", "DonkeyKong64Randomizer", "N64", ConnectorType.N64Connector);
+
+    public override EffectList Effects { get; } = new List<Effect>
+    {
+        new("Drunk Kong","drunky_kong") { Price = 0, Duration = 30, Description = "Makes the kong feel a little woozy. Reverses controls and slows down the player.", Category="Player" },
+    };
+
+    public override ROMTable ROMTable => new[]
+    {
+        new ROMInfo("Donkey Kong 64 Randomizer", "DonkeyKong64Randomizer.bps", Patching.BPS, ROMStatus.ValidUnpatched, s => Patching.MD5(s,"9ec41abf2519fc386cadd0731f6e868c"))
+    };
+
+    protected override GameState GetGameState()
+    {
+        if (!Connector.Connected) return GameState.Unknown;
+        //probably want to do a general state check here?
+        //there are A LOT of GameState return values to choose from so be sure to pick the right one
+        //nothing works unless you return GameState.Ready, every other state is just an informational failure state
+
+        //if (!ADDR_CC_STATE.TryGetByte(out byte value)) return GameState.Unmodded;
+        //return (value == (int)CC_STATE.CC_READY) ? GameState.Ready : GameState.PipelineBusy;
+        return GameState.Ready;
+    }
+
+    public override List<string> MetadataCommon { get; } = ["lives"];
+
+    protected override async Task<DataResponse> RequestData(string key)
+    {
+        switch (key)
+        {
+            case "lives":
+                //read out whatever value you want here
+                return DataResponse.Success(key, 0);
+            default:
+                return await base.RequestData(key);
+        }
+    }
+
+    protected override void StartEffect(EffectRequest request)
+    {
+        switch (request.EffectID)
+        {
+            case "drunky_kong":
+                TryEffect(request,
+                    () => Connector.IsEqual8(DRUNKY_STATE, (byte)CC_STATE.CC_READY),
+                    () => DRUNKY_STATE.TrySetByte((byte)CC_STATE.CC_ENABLING));
+                return;
+            default:
+                Respond(request, EffectStatus.FailPermanent, StandardErrors.UnknownEffect, request);
+                return;
+        }
+    }
+
+    protected override bool StopEffect(EffectRequest request)
+    {
+        switch (request.EffectID)
+        {
+            case "drunky_kong":
+                //set some value back to normal for effects that need turning off
+                //this is the preferred method! - it's always preferable to let CC
+                //(and thus the streamer) be in control of an effect's duration
+                //rather than relying on fixed timers
+
+                //return ADDR_CC_STATE.TrySetByte((byte)CC_STATE.CC_ENABLING))
+                return true;
+            default:
+                return base.StopEffect(request);
+        }
+    }
+
+    public override bool StopAllEffects()
+    {
+        bool result = base.StopAllEffects();
+        //something here
+        return result;
+    }
+}
