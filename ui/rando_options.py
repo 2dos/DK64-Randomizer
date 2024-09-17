d9e5afee7bc194f7079e2a59df1a6648df3dcc1f
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2024-09-16 21:43:37 -0400

    migrate more items

--- ui/rando_options.py
+++ ui/rando_options.py
@@ -1,12 +1,7 @@
 """Options for the main rando tab."""
-
-import random
-import re
-
 import js
 
-from randomizer.Lists.Songs import MusicSelectionPanel
-from ui.bindings import bind, bindList
+from ui.bindings import bind
 from ui.randomize_settings import randomize_settings
 
 
@@ -19,21 +14,3 @@ def shuffle_settings(evt):
     # Run additional functions to ensure there are no conflicts.
     js.update_ui_states(evt)
 
-
-musicToggles = [category.replace(" ", "") for category in MusicSelectionPanel.keys()]
-
-
-@bindList("click", musicToggles, suffix="_collapse_toggle")
-def toggle_collapsible_container(evt):
-    """Show or hide a collapsible container."""
-    targetElement = evt.target
-    if "collapse_toggle" not in targetElement.id:
-        # Get the parent of this element.
-        targetElement = targetElement.parentElement
-    toggledElement = re.search("^(.+)_collapse_toggle$", targetElement.id)[1]
-    """Open or close the settings table on the Seed Info tab."""
-    settingsTable = js.document.getElementById(toggledElement)
-    settingsTable.classList.toggle("collapsed")
-    toggledArrow = f'{toggledElement.replace("_", "-")}-expand-arrow'
-    settingsArrow = js.document.getElementsByClassName(toggledArrow).item(0)
-    settingsArrow.classList.toggle("flipped")
