3f279e8e2208c8b7957b44dac4a3ebf312c4bccd
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2025-01-20 15:53:58 -0500

    Create Modal for loading

--- static/js/rando_options.js
+++ static/js/rando_options.js
@@ -686,6 +686,8 @@ function toggle_medals_box() {
 async function enable_plandomizer() {
   const plandoTab = document.getElementById("nav-plando-tab");
   if (document.getElementById("enable_plandomizer").checked) {
+    // Open up a Modal stating that we're loading the Plando tab
+    $("#plando-modal").modal("show");    
     try {
       await setup_pyodide();
     } catch (error) {
@@ -694,6 +696,7 @@ async function enable_plandomizer() {
     // Load ui.__init__.py
     await run_python_file("ui/__init__.py");
     plandoTab.style.display = "";
+    $("#plando-modal").modal("hide");
   } else {
     plandoTab.style.display = "none";
   }
