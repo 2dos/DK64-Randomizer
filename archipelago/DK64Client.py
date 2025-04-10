3c2c4d29c838897bedab4b99419c050a73ad6c34
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2025-04-10 18:35:17 -0400

    Various Fixes

--- archipelago/DK64Client.py
+++ archipelago/DK64Client.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
 import json
 import asyncio
 import colorama
+import sys
 import time
 import typing
 from client.common import DK64MemoryMap, create_task_log_exception, check_version
@@ -542,8 +543,20 @@ class DK64Context(CommonContext):
 
         super().__init__(server_address, password)
 
+    def already_running(self) -> bool:
+        try:
+            import ctypes
+
+            mutex = ctypes.windll.kernel32.CreateMutexW(None, 1, "DK64_GUI_MUTEX")
+            return ctypes.GetLastError() == 183  # ERROR_ALREADY_EXISTS
+        except Exception:
+            return False
+
     def run_gui(self) -> None:
         """Run the GUI."""
+        if self.already_running():
+            print("GUI already running.")
+            sys.exit(1)
         from kvui import GameManager
 
         class DK64Manager(GameManager):
