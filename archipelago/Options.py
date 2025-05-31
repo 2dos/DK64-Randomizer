35f9ee1251df1aa2032e9890c2f084c558a198c2
Thomas Vatter <11064610+Killklli@users.noreply.github.com>
2025-05-30 21:01:29 -0400

    This is blursed (#2628)

--- archipelago/Options.py
+++ archipelago/Options.py
@@ -250,12 +250,24 @@ class RingLink(Toggle):
     display_name = "Ring Link"
 
 
+class TagLink(Toggle):
+    """Determines if the Tag Link is enabled.
+
+    If enabled, if you have another players kong you will tag to that kong as well.
+    If you don't have that kong, you will randomly tag to another kong.
+    If you only have one kong, nothing will happen.
+    """
+
+    display_name = "Tag Link"
+
+
 @dataclass
 class DK64Options(PerGameCommonOptions):
     """Options for DK64R."""
 
     death_link: DeathLink
     ring_link: RingLink
+    tag_link: TagLink
     goal: Goal
     krool_key_count: KeysRequiredToBeatKrool
     krool_phase_count: KroolPhaseCount
