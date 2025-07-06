bf3bf828b20ba5be3bbb4883dae0daa0fbc4b018
Ballaam <theballaam96@gmail.com>
2025-07-06 14:00:36 -0500

    Revert "Fix bug with Archipelago 0.6.1 client for linux where apworld root path is not added to syspath"

--- __init__.py
+++ __init__.py
@@ -111,12 +111,10 @@ if baseclasses_loaded:
     else:
         raise Exception(f"Unsupported platform: {platform_type}")
 
-    cwd = os.path.dirname(__file__)
-    sys.path.append(cwd)
-    sys.path.append(cwd + "/" + "worlds/dk64/")
-    sys.path.append(cwd + "/" + "worlds/dk64/archipelago/")
-    sys.path.append(cwd + "/" + "custom_worlds/dk64.apworld/dk64/")
-    sys.path.append(cwd + "/" + "custom_worlds/dk64.apworld/dk64/archipelago/")
+    sys.path.append("worlds/dk64/")
+    sys.path.append("worlds/dk64/archipelago/")
+    sys.path.append("custom_worlds/dk64.apworld/dk64/")
+    sys.path.append("custom_worlds/dk64.apworld/dk64/archipelago/")
 
     import randomizer.ItemPool as DK64RItemPool
 
