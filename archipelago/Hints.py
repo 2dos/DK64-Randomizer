37e436b702d145cd1f4287b355c3980cc0d01bf3
AmedMusl <124306572+AmedMusl@users.noreply.github.com>
2025-07-26 21:35:32 -0500

    grammar

--- archipelago/Hints.py
+++ archipelago/Hints.py
@@ -122,9 +122,9 @@ def parseKeyHint(world, location):
     """Write a key hint for the given location."""
     text = ""
     if location.player != world.player:
-        text = f"Your \x07{location.item.name[:40]}\x07 is locked away by \x05{world.multiworld.get_player_name(location.player)}\x05 in \x0d{location.name[:80]}\x0d.".upper()
+        text = f"\x07{location.item.name[:40]}\x07 is hidden for \x05{world.multiworld.get_player_name(location.player)}\x05 to find in \x0d{location.name[:80]}\x0d.".upper()
     else:
-        text = f"\x07{location.item.name[:40]}\x07 is locked away in \x0d{location.name}\x0d.".upper()
+        text = f"\x07{location.item.name[:40]}\x07 is hidden away in \x0d{location.name}\x0d.".upper()
     for letter in text:
         if letter not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?:;'S-()% \x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d":
             text = text.replace(letter, " ")
@@ -137,7 +137,7 @@ def parseKongHint(world, location):
     if location.player != world.player:
         text = f"\x07{location.item.name[:40]}\x07 is to be found by \x05{world.multiworld.get_player_name(location.player)}\x05 in \x0d{location.name[:80]}\x0d.".upper()
     else:
-        text = f"\x07{location.item.name[:40]}\x07 can be found in \x0d{location.name}\x0d.".upper()
+        text = f"\x07{location.item.name[:40]}\x07  \x0d{location.name}\x0d.".upper()
     for letter in text:
         if letter not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?:;'S-()% \x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d":
             text = text.replace(letter, " ")
