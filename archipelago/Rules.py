6a765549b2e9690f977da1e31b06249e26395acc
GreenestBeen <ameyers@alumni.cmu.edu>
2025-12-20 23:03:45 -0500

    Cleanup 1: Rules.py

--- archipelago/Rules.py
+++ archipelago/Rules.py
@@ -1,9 +1,6 @@
 """Rules for Archipelago."""
 
 from BaseClasses import MultiWorld
-from worlds.AutoWorld import LogicMixin
-from worlds.generic.Rules import add_rule, set_rule
-
 
 def set_rules(world: MultiWorld, player: int):
     """Set the rules for the given player's world."""
